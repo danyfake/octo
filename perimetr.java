de5dde07502010c9fc07baa391ccb1f7805433e0
danyfake <flavouri@mail.ru>
2015-04-22 03:47:24 +0400

    Add perimetr program

---
+++ perimetr.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class perimetr {
+	public static void main(String[] args){ 
+		System.out.println("Enter integer number: "); 
+		int a = new Scanner(System.in).nextInt();
+		System.out.println("Enter integer number: "); 
+		int b = new Scanner(System.in).nextInt();
+		if (a<b){
+			System.out.println("Max value is "+b);}
+		else {if (a>b){
+			System.out.println("Max value is "+a);}
+		else {System.out.println("omg they're equal");}
+		}
+	} 
+}
