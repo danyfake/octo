c63ed6ed7cfc2c15c654eb301ce351eed30e6e13
Darya Ov <flavouri@mail.ru>
2015-04-24 00:19:35 +0400

    Update perimetr.java

--- perimetr.java
+++ perimetr.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
 //app for calculating perimeter of square or circle
-public class perimetr {
+public class Perimetr {
 	public static void main(String[] args){ 
 		System.out.println("Enter 0-square, 1-circle: "); 
 		int a = new Scanner(System.in).nextInt();
 		switch (a){
-		case 0:Psquare(); break; 
-		case 1:Pcircle(); break;
+		case 0:psquare(); break; 
+		case 1:pcircle(); break;
 		default: System.out.println("Out of scope");
 		}
 	} 
 	//calculating perimeter of square
-	public static void Psquare(){
+	public static void psquare(){
 		System.out.println("Enter the length of the sides of the square: "); 
 		int a = new Scanner(System.in).nextInt();
 		int p=4*a;
 		System.out.println("Perimeter = " + p);
 	}
 	//calculating perimeter of circle
-	public static void Pcircle(){
+	public static void pcircle(){
 		System.out.println("Enter the radius of the circle: "); 
 		int a = new Scanner(System.in).nextInt();
 		double p= 2 * a * 3.14;
